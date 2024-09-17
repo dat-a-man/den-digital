480d39e440e4ec88618f5441e045b8ad4e0ca8a9
akashpatelknit <akash.20606@knit.ac.in>
2024-09-17 21:15:01 +0530

    date fixed

--- app/(web)/post/[slug]/_components/BlogPost.jsx
+++ app/(web)/post/[slug]/_components/BlogPost.jsx
@@ -194,7 +194,7 @@ const BlogPost = () => {
                 {post.author.name}
               </p>
               <p className="text-xs text-gray-500 dark:text-gray-400">
-                {dayjs(post._updatedAt).format("MMMM DD, YYYY")}
+                {dayjs(post.publishedAt).format("MMMM DD, YYYY")}
               </p>
             </div>
             <div className="flex items-center gap-4 my-2">
